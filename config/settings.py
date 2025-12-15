82d175e81e2818ead139c6a96d78e2eaedc1fd6a
Ilhomjon Murodov <imurodov247@gmail.com>
2025-12-15 11:06:01 +0500

    inital

--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
-     'rest_framework_simplejwt',
-     'corsheaders',
+    'rest_framework_simplejwt',
+    'corsheaders',
     
 ]
 
